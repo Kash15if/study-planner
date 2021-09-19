420f867e24ff2da61183d43a4fc80baebd0278aa
Kash15if <kash15if@yahoo.com>
2021-09-19 22:29:25 +0530

    first commit

---
+++ src/Theme.js
@@ -0,0 +1,43 @@
+import { createTheme } from "@mui/material/styles";
+
+const lightTheme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      light: "#757ce8",
+      main: "#42a5f5",
+      dark: "#BB86FC",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#42a5f5",
+      dark: "#03DAC6",
+      contrastText: "#000",
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#BB86FC",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#03DAC6",
+      contrastText: "#000",
+    },
+    background: {
+      paper: "#0D1B2A",
+      default: "#0D1B2A",
+    },
+  },
+});
+
+export { darkTheme, lightTheme };
