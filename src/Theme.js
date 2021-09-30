12f4082018ffae189186a60b7a3085cad5a07cc7
Kash15if <kash15if@yahoo.com>
2021-09-30 23:06:29 +0530

    correction in theme from type to mode

--- src/Theme.js
+++ src/Theme.js
@@ -22,25 +22,24 @@ const darkTheme = createTheme({
   palette: {
     mode: "dark",
     primary: {
-      light: "#BB86FC",
+      light: "#0D1B2A",
       main: "#BB86FC",
       dark: "#BB86FC",
-      contrastText: "#BB86FC",
+      contrastText: "#0D1B2A",
     },
     secondary: {
-      light: "#03DAC6",
+      light: "#0D1B2A",
       main: "#03DAC6",
-      dark: "#03DAC6",
-      contrastText: "#03DAC6",
+      dark: "#0D1B2A",
+      contrastText: "#0D1B2A",
     },
     background: {
       paper: "#0D1B2A",
       default: "#0D1B2A",
-      contrastText: "#BB86FC",
     },
     text: {
       primary: "#BB86FC",
-      secondary: "#03DAC6",
+      secondary: "#BB86FC",
       disabled: "red",
       icon: "#BB86FC",
       divider: "#03DAC6",
