cced7680f2b49ccfb353181c69bb4123e300196b
Kash15if <kash15if@yahoo.com>
2021-10-15 20:15:10 +0530

    create task table width reduced

--- src/Pages/CreateTask.js
+++ src/Pages/CreateTask.js
@@ -1,12 +1,23 @@
 import CreateTask from "../Components/CreateTaskForm";
 import EditableTable from "../CustomComponents/EditableTable";
+import { Grid } from "@mui/material";
 
 const CreateNewTask = () => {
   return (
     <div>
       <CreateTask />
 
-      <EditableTable />
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item md={8} xs={12}>
+          <EditableTable />
+        </Grid>
+      </Grid>
     </div>
   );
 };
