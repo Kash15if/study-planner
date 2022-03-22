981bd70186b64c0299dbcd51aa838ad316ae80f6
Kash15if <kash15if@yahoo.com>
2022-03-22 23:42:12 +0530

    added table in create subject and also added editing feature. Delete and save to be implemented next

---
+++ src/Components/SubjectTable.js
@@ -0,0 +1,104 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { visuallyHidden } from "@mui/utils";
+import { useState } from "react";
+
+export default function EnhancedTable(props) {
+  const data = [
+    {
+      subject: "abvf",
+      date: "fef",
+    },
+    {
+      subject: "pqr",
+      date: "dddddddddddddddd",
+    },
+  ];
+
+  const handleClick = (event, rowData) => {
+    let d = new Date();
+    let dateIn = d.toLocaleDateString();
+    props.handleEdit({
+      ...rowData,
+      date: dateIn,
+    });
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2, p: 3 }}>
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size="medium"
+          >
+            <TableHead p={5}>
+              <TableRow>
+                <TableCell key="subject" padding="2rem">
+                  <Typography variant="subtitle1" style={{ fontWeight: 600 }}>
+                    Subject
+                  </Typography>
+                </TableCell>
+
+                <TableCell key="date" padding="2rem">
+                  <Typography variant="subtitle1" style={{ fontWeight: 600 }}>
+                    Date Created
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {data.map((row, index) => {
+                return (
+                  <TableRow
+                    hover
+                    onClick={(event) => handleClick(event, row)}
+                    // aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={row.name}
+                  >
+                    <TableCell
+                      component="th"
+                      id="subject"
+                      scope="row"
+                      padding="2rem"
+                    >
+                      {row.subject}
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      id="date"
+                      scope="row"
+                      padding="2rem"
+                    >
+                      {row.date}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </Box>
+  );
+}
--- src/Pages/CreateSubject.js
+++ src/Pages/CreateSubject.js
@@ -1,9 +1,19 @@
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
-import SubjectTable from "../CustomComponents/EditableTable";
+import SubjectTable from "../Components/SubjectTable";
 import { Grid } from "@mui/material";
+import { useState } from "react";
 
 const CreateSubject = () => {
+  const [formData, setFormData] = useState({
+    subject: "",
+    date: "",
+  });
+
+  const handleFormEdit = (val) => {
+    console.log(val);
+    setFormData(val);
+  };
   return (
     <div>
       <Grid
@@ -15,7 +25,12 @@ const CreateSubject = () => {
         style={{ marginBottom: "5rem" }}
       >
         <Grid item md={3} sm={12}>
-          <TextField id="outlined-basic" label="Subject" variant="outlined" />
+          <TextField
+            id="outlined-basic"
+            label="Subject"
+            variant="outlined"
+            value={formData.subject}
+          />
         </Grid>
 
         <Grid item md={2} sm={12}>
@@ -24,7 +39,19 @@ const CreateSubject = () => {
           </Button>
         </Grid>
       </Grid>
-      <SubjectTable />
+
+      <Grid
+        container
+        spacing={5}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        style={{ marginBottom: "5rem" }}
+      >
+        <Grid item md={8} sm={12}>
+          <SubjectTable handleEdit={handleFormEdit} />
+        </Grid>
+      </Grid>
     </div>
   );
 };
