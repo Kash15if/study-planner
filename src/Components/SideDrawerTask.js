a19e5c2480e409824b04bdaea1c15dd42806e7da
Kash15if <kash15if@yahoo.com>
2022-03-31 08:28:38 +0530

    added close button but not logic and grid to task details

--- src/Components/SideDrawerTask.js
+++ src/Components/SideDrawerTask.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Box, Button } from "@mui/material";
 import { Grid } from "@mui/material";
+import CancelPresentationIcon from "@mui/icons-material/CancelPresentation";
 
 import SortableTable from "../CustomComponents/SortableTable";
 
@@ -26,45 +27,56 @@ const DrawerTask = (props) => {
   const columns = [
     { field: "id", hide: true },
     {
-      field: "firstName",
-      headerName: "First name",
-      width: 150,
+      field: "subtask",
+      headerName: "SubTask",
+      minWidth: 150,
+      flex: 1,
       editable: true,
     },
     {
-      field: "lastName",
-      headerName: "Last name",
-      width: 150,
+      field: "desc",
+      headerName: "Description",
+      minWidth: 150,
+      flex: 1,
       editable: true,
     },
     {
-      field: "age",
-      headerName: "Age",
+      field: "link",
+      headerName: "URL",
       type: "number",
-      width: 110,
+      minWidth: 150,
+      flex: 1,
       editable: true,
     },
     {
-      field: "fullName",
-      headerName: "Full name",
+      field: "comp",
+      headerName: "Completed",
       description: "This column has a value getter and is not sortable.",
       sortable: false,
-      width: 160,
+      flex: 1,
+      minWidth: 150,
       valueGetter: (params) =>
         `${params.row.firstName || ""} ${params.row.lastName || ""}`,
     },
   ];
 
   const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 1, subtask: "Snow", desc: "Jon", link: 35, comp: true },
+    { id: 2, subtask: "Snow", desc: "dvdddd dfd fefe", link: 35, comp: true },
+    {
+      id: 3,
+      subtask: "Snow",
+      desc: "fe fefe feads fvdvxczx axzc",
+      link: 35,
+      comp: false,
+    },
+    {
+      id: 4,
+      subtask: "Snow",
+      desc: "scse vrf xz vfap lad l ",
+      link: 35,
+      comp: true,
+    },
   ];
 
   return (
@@ -79,7 +91,7 @@ const DrawerTask = (props) => {
             backgroundColor: "yellow",
           }}
         >
-          close
+          <CancelPresentationIcon fontSize="large" />
         </Button>
         <table>
           <tr>
